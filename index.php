281f3866d91bb50e94eb9940c392b7a2d5e88ef1
Arjun ghimire <arjunghimire1210@gmail.com>
2022-04-20 20:17:01 +0545

    Code completed

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Website</title>
+</head>
+<body>
+    <form action="processcode.php" method="post">
+
+    <textarea name="code" id="code" cols="60" rows="30"></textarea>
+    <br>
+    <input type="submit">
+    </form>
+</body>
+</html>
